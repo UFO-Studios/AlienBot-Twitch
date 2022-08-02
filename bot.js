b25bca30ef5ef6541c2521cab627c77167915e09
Niceygy <niceygy@niceygylive.xyz>
2022-08-02 09:36:22 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+const TwitchBot = require('twitch-bot')
+ 
+const Bot = new TwitchBot({
+  username: 'Kappa_Bot',
+  oauth: 'oauth:dwiaj91j1KKona9j9d1420',
+  channels: ['twitch']
+})
+ 
+Bot.on('join', channel => {
+  console.log(`Joined channel: ${channel}`)
+})
+ 
+Bot.on('error', err => {
+  console.log(err)
+})
+ 
+Bot.on('message', chatter => {
+  if(chatter.message === '!test') {
+    Bot.say('Command executed! PogChamp')
+  }
+})
